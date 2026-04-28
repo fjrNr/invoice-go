db0bc23626620c0d5cb6b7c3ec02bdcf8bb13887
fjrNr <fajar-n@outlook.com>
2026-04-28 11:08:19 +0700

    fix: route for guest mode

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -4,17 +4,24 @@ import { NextResponse } from 'next/server'
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
-  const redirectTo = searchParams.get('redirect_to') ?? '/dashboard'
+  const next = searchParams.get('next') ?? '/dashboard'
 
   if (code) {
     const supabase = await createClient()
     const { error } = await supabase.auth.exchangeCodeForSession(code)
-
     if (!error) {
-      return NextResponse.redirect(`${origin}${redirectTo}`)
+      const forwardedHost = request.headers.get('x-forwarded-host')
+      const isLocalEnv = process.env.NODE_ENV === 'development'
+      if (isLocalEnv) {
+        return NextResponse.redirect(`${origin}${next}`)
+      } else if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`)
+      } else {
+        return NextResponse.redirect(`${origin}${next}`)
+      }
     }
   }
 
   // Return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/login?error=auth_code_error`)
+  return NextResponse.redirect(`${origin}/auth/auth-code-error`)
 }
